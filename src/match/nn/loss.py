878e946dedd46a15ef405714e3dd21222f8feae7
Sam <sameerrajmalik@gmail.com>
2025-03-15 15:54:33 -0700

    Implement cross entropy loss

--- src/match/nn/loss.py
+++ src/match/nn/loss.py
@@ -9,3 +9,13 @@ class MSELoss(Module):
     def forward(self, prediction: Tensor, target: Tensor) -> Tensor:
         # Returns a new Tensor
         return ((target - prediction) ** 2).mean()
+
+
+class MultiClassCrossEntropyLoss(Module):
+    """
+    loss = - (1/N) * Σ Σ yi * log(yhati)
+    """
+
+    def forward(self, prediction: Tensor, target: Tensor) -> float:
+
+        return -(target * prediction.log()).mean()
